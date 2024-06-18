1d5f90cd822b2adb979b749c6c52f743801768e8
itaya_daiya <itaya.daiya@lib-gate.co.jp>
2024-06-18 15:30:56 +0900

    lesson3&lesson4_finished

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: 'Socks'
+      product: 'Socks',
+      image: './assets/images/socks_green.jpg',
+      inventory: 100
     }
   }
 })
